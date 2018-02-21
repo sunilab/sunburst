d5094784a4ff54861f247813aef866f8cac65db9
sunilab <abhyankar_sunil@hotmail.com>
2018-02-21 11:41:22 -0800

    Added main App component, split webpack config into dev and prod, added webpack dev server capability and separate configuration for prod and dev

---
+++ app/app.js
@@ -0,0 +1,14 @@
+// Main React application entry point
+
+import React from 'react';
+
+class App extends React.Component {
+    render () {
+        console.log('hello from react');
+        return (
+            <h1>The Dashboard App now with Webpack enhancements and more!</h1>
+        );
+    }
+}
+
+export default App;
